f2a81aa03cb232193e3345012ae09172db1b3de1
Michael Orji <orjimichael4886@gmail.com>
2023-02-09 06:17:33 -0800

    updated app, controller and data files

--- app.js
+++ app.js
@@ -1,41 +1,44 @@
 //a module imported from the node_modules folder, is always inside double quotes and without slashes or dots
 const http = require("http");
-const Todo = require("./controller");
+const Movie = require("./controller");
 const { getReqData } = require("./utils");
 
 const PORT = process.env.PORT || 5000;
 
-// /api/v1/todos: GET
+// /api/v1/movies: GET
 const server = http.createServer(async (req, res) => {
-    
+
     //default
-    if (req.url === "/api/v1/todos" && req.method === "GET") {
-        //get the todos
-        const todos = await Todo.getTodos();
+    if (req.url === "/api/v1/movies" && req.method === "GET") {
+
+        //get the movies
+        const movies = await Movie.getMovies();
 
         //set the status code and content-type
         res.writeHead(200, { "Content-Type": "application/json" });
 
+        // res.end(JSON.stringify({ message: "Route not found" }));
+
         //send the data
-        res.end(JSON.stringify(todos));
+        res.end(JSON.stringify(movies));
     }
 
 
-    // /api/v1/todos:id: GET
-    else if (req.url.match(/\/api\/v1\/todos\/([0-9]+)/) && req.method === "GET") {
+    // /api/v1/movies:id: GET
+    else if (req.url.match(/\/api\/v1\/movies\/([0-9]+)/) && req.method === "GET") {
         try {
 
             //get id from url
             const id = req.url.split("/")[4];
 
-            //get todo
-            const todo = await Todo.getTodo(id);
+            //get movie
+            const movie = await Movie.getMovie(id);
 
             //set the status code and content-type
             res.writeHead(200, { "Content-Type": "application/json" });
 
             //send the data
-            res.end(JSON.stringify(todos));
+            res.end(JSON.stringify(movie));
         } catch (error) {
 
             //set the status code and content-type
@@ -46,15 +49,15 @@ const server = http.createServer(async (req, res) => {
         }
     }
 
-    // /api/v1/todos:id: DELETE
-    else if (req.url.match(/\/api\/v1\/todos\/([0-9]+)/) && req.method === "DELETE") {
+    // /api/v1/movies:id: DELETE
+    else if (req.url.match(/\/api\/v1\/movies\/([0-9]+)/) && req.method === "DELETE") {
         try {
 
             //get id from url
             const id = req.url.split("/")[4];
 
-            //delete todo
-            const message = await Todo.deleteTodo(id);
+            //delete movie
+            const message = await Movie.deleteMovie(id);
 
             //set the status code and content-type
             res.writeHead(200, { "Content-Type": "application/json" });
@@ -71,21 +74,21 @@ const server = http.createServer(async (req, res) => {
         }
     }
 
-    // /api/v1/todos:id: UPDATE
-    else if (req.url.match(/\/api\/v1\/todos\/([0-9]+)/) && req.method === "PATCH") {
+    // /api/v1/movies:id: UPDATE
+    else if (req.url.match(/\/api\/v1\/movies\/([0-9]+)/) && req.method === "PATCH") {
         try {
 
             //get id from url
             const id = req.url.split("/")[4];
 
-            //update todo
-            let updatedTodo = await Todo.updateTodo(id);
+            //update movie
+            let updatedMovie = await Movie.updateMovie(id);
 
             //set the status code and content-type
             res.writeHead(200, { "Content-Type": "application/json" });
 
             //send the data
-            res.end(JSON.stringify(updatedTodo));
+            res.end(JSON.stringify(updatedMovie));
         } catch (error) {
 
             //set the status code and content-type
@@ -96,20 +99,20 @@ const server = http.createServer(async (req, res) => {
         }
     }
 
-    // /api/v1/todos:id: POST
-    else if (req.url === "/api/v1/todos" && req.method === "POST") {
+    // /api/v1/movies:id: POST
+    else if (req.url === "/api/v1/movies" && req.method === "POST") {
 
         //get all data
-        let todoData = await getReqData(req);
+        let movieData = await getReqData(req);
 
-        //create todo
-        let todo = await Todo.createTodo(JSON.parse(todoData));
+        //create movie
+        let movie = await Movie.createMovie(JSON.parse(movieData));
 
         //set the status code and content-type
         res.writeHead(201, { "Content-Type": "application/json" });
 
         //send the data
-        res.end(JSON.stringify(todo));
+        res.end(JSON.stringify(movie));
 
     }
 
@@ -117,6 +120,7 @@ const server = http.createServer(async (req, res) => {
     else {
         //set the status code and content-type
         res.writeHead(404, { "Content-Type": "application/json" });
+
 
         //send the error
         res.end(JSON.stringify({ message: "Route not found" }));
